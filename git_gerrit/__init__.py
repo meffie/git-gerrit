b4ca901d85d579956049e5be92d65473abaa0446
Michael Meffie <mmeffie@sinenomine.net>
2018-08-11 13:24:15 -0400

    Export our custom exceptions

--- git_gerrit/__init__.py
+++ git_gerrit/__init__.py
@@ -1,10 +1,14 @@
 from git_gerrit._version import __version__
-from git_gerrit._fetch import fetch
+from git_gerrit._cfg import GerritError, GerritConfigError
+from git_gerrit._fetch import fetch, GerritNotFoundError
 from git_gerrit._log import log
 from git_gerrit._query import query
 
 _hush_pyflakes = [
     __version__,
+    GerritError,
+    GerritConfigError,
+    GerritNotFoundError,
     fetch,
     log,
     query,
